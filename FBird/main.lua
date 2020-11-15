f663af2a6f1b27dceef63a172325ea727f8e0cc9
Raymond Christian P. Soriano <72679224+rcpsoriano@users.noreply.github.com>
2020-11-15 13:15:02 +0800

    Pipe Update~

--- FBird/main.lua
+++ FBird/main.lua
@@ -1,6 +1,5 @@
---update-2
---bird update-
-
+--Pipe update-
+--update 5
 
 
 -- to load push.lua
@@ -8,6 +7,8 @@
 push = require 'push'
 
 Class = require 'class'
+
+require 'Pipe'
 
 require 'Bird'
 WINDOW_WIDTH = 1280
@@ -28,12 +29,23 @@ local GROUND_SCROLL_SPEED = 60
 local BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
+
+local pipes = {}
+
+local spawnTimer = 0
+
+
+
 function love.load()
     
     
     love.graphics.setDefaultFilter('nearest','nearest')
 
+    math.randomseed(os.time())
+
+    --States the window title.
     love.window.setTitle('Kluk Bird')
+
 
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -41,6 +53,8 @@ function love.load()
         fullscreen = false,
         resizable = true
     })
+
+    love.keyboard.keysPressed = {}
 end 
 
 --function that allows the user to resize the window of the game
@@ -50,10 +64,21 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+    
+    
     if key == 'escape' then
         love.event.quit()
     end
 end
+
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end    
 
 
 function love.update(dt)
@@ -63,17 +88,42 @@ function love.update(dt)
     
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
         %GROUND_SCROLL_SPEED
+
+    spawnTimer = spawnTimer + dt
+
+    if spawnTimer > 2 then
+        table.insert(pipes, Pipe())
+        spawnTimer = 0
+    end
+    
+    
+    bird:update(dt)   
+
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+
+        if pipe.x < -pipe.width then
+            table.remove(pipes, k)
+        end
+    end
+
+    --reset input
+    love.keyboard.keysPressed = {}
+
 end
+
 function love.draw()
     push:start()
     love.graphics.draw(background, -backgroundScroll, 0)
     
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT -16)
     
     bird:render()
     
     push:finish()
-
 
 
 end
