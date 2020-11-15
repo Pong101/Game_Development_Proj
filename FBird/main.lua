fa3753e33743d6e5ced1dc9e82d944e5b96ea1f9
Raymond Christian P. Soriano <72679224+rcpsoriano@users.noreply.github.com>
2020-11-15 11:43:53 +0800

    Birdy Update

--- FBird/main.lua
+++ FBird/main.lua
@@ -1,4 +1,5 @@
---update-0
+--update-2
+--bird update-
 
 
 
@@ -6,6 +7,9 @@
 
 push = require 'push'
 
+Class = require 'class'
+
+require 'Bird'
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -13,12 +17,23 @@ VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
-local ground = love.graphics.newImage('ground.png')
+local backgroundScroll = 0
 
+local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
 function love.load()
+    
+    
     love.graphics.setDefaultFilter('nearest','nearest')
 
-    love.window.setTitle('Fly Bird')
+    love.window.setTitle('Kluk Bird')
 
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -40,11 +55,25 @@ function love.keypressed(key)
     end
 end
 
+
+function love.update(dt)
+    --scroll background preset
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+        %BACKGROUND_LOOPING_POINT
+    
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+        %GROUND_SCROLL_SPEED
+end
 function love.draw()
     push:start()
-    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(background, -backgroundScroll, 0)
     
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT -16)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT -16)
+    
+    bird:render()
+    
     push:finish()
+
+
 
 end
