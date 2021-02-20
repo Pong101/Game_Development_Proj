23cdfd6fb5f48c04ab3d23352bf4bbc6d778d8cf
Raymond Christian P. Soriano <72679224+rcpsoriano@users.noreply.github.com>
2021-02-20 15:37:39 +0800

    Update conf.lua

--- Johhny's Adventure at Demon Island/conf.lua
+++ Johhny's Adventure at Demon Island/conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.title = "Johhny's Adventure at Demon Island"
     t.version = "11.3"
-    t.console = true
+    t.console = false
     t.window.width = 1280
     t.window.height = 720
     t.window.vsync = 0
